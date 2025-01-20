14dd8d044dc3bc0ef3bcb60407f95b3de66afbf3
Max Demydenko <demkom58@gmail.com>
2025-01-20 12:37:06 +0200

    chore: remove init scripts

--- app/downloaders/__init__.py
+++
@@ -1,5 +0,0 @@
-from .tiktok import TikTokDownloader
-from .youtube import YouTubeShortsDownloader
-from .coub import CoubDownloader
-
-__all__ = ['TikTokDownloader', 'YouTubeShortsDownloader', 'CoubDownloader']
